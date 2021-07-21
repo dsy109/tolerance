238c9e4979ae32e06e049dced20fdf4ba90e9a43
Kedai Cheng <74564639+Kedai-Cheng@users.noreply.github.com>
2021-07-20 23:16:11 -0400

    Update regtol.int2.R

--- Updated Functions/regtol.int2.R
+++ Updated Functions/regtol.int2.R
@@ -59,7 +59,7 @@ regtol.int2 <- function (reg, new.x = NULL, side = 1, alpha = 0.05, P = 0.99, ne
     temp2 <- list()
     temp2$tol <- temp[,3:6]
     temp2$alpha.P <- c(alpha,P)
-    temp2$reg.type  <- "linear regression"
+    temp2$reg.type  <- "linear_regression"
     temp2$model <- reg
     temp2
   } else{
