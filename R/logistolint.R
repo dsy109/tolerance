169bfcfdd477b29030e1086f0244abc894bce9cf
Derek S. Young <54218160+dsy109@users.noreply.github.com>
2024-06-17 10:41:42 -0400

    Added a more stable option for computing limits.
    Thanks to Cameron Willden for the contributed "BE" option code.

--- R/logistolint.R
+++ R/logistolint.R
@@ -1,10 +1,11 @@
 logistol.int <- function (x, alpha = 0.05, P = 0.99, log.log = FALSE,
-                           side = 1) 
+                          side = 1, method = c("HALL", "BE")) 
 {
   if (side != 1 && side != 2) {
     stop(paste("Must specify a one-sided or two-sided procedure!", 
                "\n"))
   }
+  method <- match.arg(method)
   if (log.log) 
     x <- log(x)
   if (side == 2) {
@@ -27,15 +28,27 @@ logistol.int <- function (x, alpha = 0.05, P = 0.99, log.log = FALSE,
     var.m <- inv.fish[1, 1]
     var.s <- inv.fish[2, 2]
     cov.ms <- inv.fish[1, 2]
-    k.delta <- qlogis(P, scale = sqrt(3)/pi)
-    t1 <- k.delta - cov.ms * qnorm(1 - alpha)^2
-    t2 <- k.delta + cov.ms * qnorm(1 - alpha)^2
-    u <- k.delta^2 - var.m * qnorm(1 - alpha)^2
-    v <- 1 - var.s * qnorm(1 - alpha)^2
-    k.lower <- (t1 + sqrt(t1^2 - u * v))/v
-    k.upper <- (t2 + sqrt(t1^2 - u * v))/v
-    L <- m - k.lower * s * pi/sqrt(3)
-    U <- m + k.upper * s * pi/sqrt(3)    
+    Z_alpha <- qnorm(1-alpha)
+    if(method == "HALL"){
+      k.delta <- qlogis(P, scale = sqrt(3)/pi)
+      t1 <- k.delta - cov.ms * Z_alpha^2
+      t2 <- k.delta + cov.ms * Z_alpha^2
+      u <- k.delta^2 - var.m * Z_alpha^2
+      v <- 1 - var.s * Z_alpha^2
+      k.lower <- (t1 + sqrt(t1^2 - u * v))/v
+      k.upper <- (t2 + sqrt(t1^2 - u * v))/v
+      L <- m - k.lower * s * pi/sqrt(3)
+      U <- m + k.upper * s * pi/sqrt(3)    
+    } else{
+      q_p <- qlogis(P)
+      C11 <- var.m/(s^2)
+      C22 <- var.s/(s^2)
+      C12 <- cov.ms/(s^2)
+      k.lower <- Z_alpha*sqrt(C11 + (q_p^2)*C22 - 2*q_p*C12)+q_p
+      k.upper <- Z_alpha*sqrt(C11 + (q_p^2)*C22 + 2*q_p*C12)+q_p
+      L <- m - k.lower * s
+      U <- m + k.upper * s
+    }
   }
   if (log.log) {
     L <- exp(L)
